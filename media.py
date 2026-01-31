4a80025a7f8396700180d6710f45368db87f410d
Rodrigo Martins <59897800+rodrigomartiins@users.noreply.github.com>
2026-01-31 14:13:40 +0000

    feat: Adicionei outra condições com for e também uma adicao de opção para aumentar a quantidade de notas

--- media.py
+++ media.py
@@ -1,22 +1,32 @@
-#Aplicação para calcular uma média entre duas notas!
-try:    
-    nota_1 = float (input('Digite a primeira nota (0/10):  '))
-    nota_2 = float (input('Digite a segunda nota (0/10): '))
+# --- Aplicação para calcular uma média entre duas notas! ---
+try:
+    quantidade = int (input('Quantas notas você gostaria de inserir? '))
     
-    if nota_1 < 0 or nota_1 > 10 or nota_2 < 0 or nota_2 > 10:
-        print('Erro: As notas devem ser entre 0 e 10.')
-    else: 
-        media = (nota_1 + nota_2) / 2
-    #Neste primeiro bloco nós isolamos a condição para que o usuário coloque os dados corretos para não quebrar a aplicação.
-        
+    if quantidade <= 0:
+        print('Erro: a quantidade deve ser maior que zero.')
+    else:
+        soma = 0
+    
+    for i in range(quantidade):
+        nota = float (input(f'Digite a nota {i + 1} (0 a 10): '))
+        if nota < 0 or nota > 10:
+            print('Erro: nota inválida.')
+            break
+    
+        soma += nota
+    
+    else:
+            
+        media = soma / quantidade
+            
         if media <= 5 :
             print(f'A sua nota foi {media:.2f}, você está Reprovado!')
         elif media >= 5 and media < 7:
             print(f'A sua nota foi {media:.2f}, você está de Recuperação!')
         else:
             print(f'A sua nota final foi de {media:.2f}, parabéns você está Aprovado!')
-        #Neste segundo bloco é a função das medias para ter o resultado final.
-        
+    # --- Neste segundo bloco é a função das medias para ter o resultado final. ---
+            
 except ValueError:
-    print('Erro: Digite apenas números')
-    #Aqui retorna a mensagem de erro caso o usuário não coloque números válidos para calcular a media.
+        print('Erro: Digite apenas números')
+# --- Aqui retorna a mensagem de erro caso o usuário não coloque números válidos para calcular a media.
