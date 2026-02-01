6046bb74616521e0968b377df25916adabd5eec3
Rodrigo Martins <59897800+rodrigomartiins@users.noreply.github.com>
2026-02-01 21:26:19 +0000

    feat: Adiciona um menu interativo no programa

--- media.py
+++ media.py
@@ -1,32 +1,50 @@
-# --- Aplicação para calcular uma média entre duas notas! ---
-try:
-    quantidade = int (input('Quantas notas você gostaria de inserir? '))
-    
-    if quantidade <= 0:
-        print('Erro: a quantidade deve ser maior que zero.')
-    else:
-        soma = 0
-    
-    for i in range(quantidade):
-        nota = float (input(f'Digite a nota {i + 1} (0 a 10): '))
-        if nota < 0 or nota > 10:
-            print('Erro: nota inválida.')
-            break
-    
-        soma += nota
-    
-    else:
-            
-        media = soma / quantidade
-            
-        if media <= 5 :
-            print(f'A sua nota foi {media:.2f}, você está Reprovado!')
-        elif media >= 5 and media < 7:
-            print(f'A sua nota foi {media:.2f}, você está de Recuperação!')
+# --- Aplicação para calcular uma média entre diversas notas! ---
+def calcular_media():
+    '''Função para o calculo da média.'''
+    try:
+        quantidade = int (input('Quantas notas você gostaria de inserir? '))
+        
+        if quantidade <= 0:
+            print('Erro: a quantidade deve ser maior que zero.')
         else:
-            print(f'A sua nota final foi de {media:.2f}, parabéns você está Aprovado!')
-    # --- Neste segundo bloco é a função das medias para ter o resultado final. ---
+            soma = 0
+        
+        for i in range(quantidade):
+            nota = float (input(f'Digite a nota {i + 1} (0 a 10): '))
+            if nota < 0 or nota > 10:
+                print('Erro: nota inválida.')
+                break
+        
+            soma += nota
+        
+        else:
+                
+            media = soma / quantidade
+                
+            if media <= 5 :
+                print(f'A sua nota foi {media:.2f}, você está Reprovado!')
+            elif media >= 5 and media < 7:
+                print(f'A sua nota foi {media:.2f}, você está de Recuperação!')
+            else:
+                print(f'A sua nota final foi de {media:.2f}, parabéns você está Aprovado!')
+# --- Neste segundo bloco é a função das medias para ter o resultado final. ---
             
-except ValueError:
+    except ValueError:
         print('Erro: Digite apenas números')
 # --- Aqui retorna a mensagem de erro caso o usuário não coloque números válidos para calcular a media.
+
+while True:
+    print('\n == MENU ==')
+    print('1 . Calcular Média')
+    print('2 . Sair')
+    
+    opcao = input('Escolha uma opção: ')
+    
+    if opcao == '1':
+        calcular_media()
+    elif opcao == '2':
+        print('Saindo do programa...')
+        break
+    else:
+        print('Opção inválida! Tente novamente.')
+    
